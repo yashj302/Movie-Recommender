79729e236de488fa5c5551d3d427f7f7a7744315
Yash Jain <yashj302@gmail.com>
2018-10-05 15:06:01 +0530

    Update Cosine_similarity.py

--- Cosine_similarity.py
+++ Cosine_similarity.py
@@ -5,12 +5,13 @@ UserID | MovieID | ratings
 
 '''
 
-
 import math
 import re
 
 cal_v = dict()
 cal_1 = dict()
+
+userID = int(input("Enter USERID:- "))
 
 dot_product = 0.0
 x = 0
@@ -31,33 +32,35 @@ def cosine_simil(cal_v,cal_1):
         sum_v += math.pow(values,2)
 
     cosine_similarity = (dot_product/(math.sqrt(sum_1) * math.sqrt(sum_v)))
-    print('COSINE SIMILARITY :-- ',cosine_similarity,'  With CUSTOMER ID:---- ',x)
+    print('COSINE SIMILARITY :-- ',round(cosine_similarity,2),'\t  With CUSTOMER ID:---- ',x)
     dot_product=0.0
 
 customer1_dictionary = dict()
 file = open('ratings.csv')
 
 for line in file:
-    if(int(line.split(',')[0])==1):
+    if(int(line.split(',')[0])==int(userID)):
         customer1_dictionary[int(line.split(',')[1])]=float(line.split(',')[2])
     else:
-        break
+        continue
 
-for x in range(2,672):
-    variable_dictionary = dict()
-    file2 = open('ratings.csv')
-    for line2 in file2:
-        if(int(line2.split(',')[0])>x):
-            break
-        elif(int(line2.split(',')[0])==x):
-            variable_dictionary[int(line2.split(',')[1])]=float(line2.split(',')[2])
+for x in range(1,672):
+    if x==userID:
+        continue
+    else:
+        variable_dictionary = dict()
+        file2 = open('ratings.csv')
+        for line2 in file2:
+            if(int(line2.split(',')[0])==x):
+                variable_dictionary[int(line2.split(',')[1])]=float(line2.split(',')[2])
+            if(int(line2.split(',')[0])>x):
+                continue
 
-    final_customer1_dict = dict()
-    final_variable_dict = dict()
-
-    for keys,values in customer1_dictionary.items():
-        if(keys in variable_dictionary):
-            final_customer1_dict[keys]=values
-            final_variable_dict[keys]=variable_dictionary[keys]
-    if(final_variable_dict):
-        cosine_simil(final_variable_dict,final_customer1_dict)
+        final_customer1_dict = dict()
+        final_variable_dict = dict()
+        for keys,values in customer1_dictionary.items():
+            if(keys in variable_dictionary):
+                final_customer1_dict[keys]=values
+                final_variable_dict[keys]=variable_dictionary[keys]
+        if(final_variable_dict):
+            cosine_simil(final_variable_dict,final_customer1_dict)
